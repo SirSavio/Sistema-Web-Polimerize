f17580993199f5910eb1181bdc15965b030b410d
paiva98 <nilsonpaiva98@gmail.com>
2021-03-29 06:02:02 -0300

    metodos para validação de dados

---
+++ backend/src/utils/celebrateMethods.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { celebrate, Segments, Joi } = require("celebrate");
+
+exports.createAdmin = {
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required().max(60),
+        email: Joi.string().required().email().max(60),
+        //Mínimo de oito caracteres, pelo menos uma letra e um número:
+        password: Joi.string().required().regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
+    }),
+}
+
+exports.changeAdmin = {
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required().max(60),
+        email: Joi.string().required().email().max(60),
+        //Mínimo de oito caracteres, pelo menos uma letra e um número:
+        password: Joi.string().required().regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
+    }),
+}
