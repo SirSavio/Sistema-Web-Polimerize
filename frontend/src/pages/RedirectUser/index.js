7cc1197fcdc2df4ae7a3bd118090d99b3ba69eca
paiva98 <nilsonpaiva98@gmail.com>
2021-04-01 17:25:14 -0300

    implementação da tela de redirecionar usuário

--- frontend/src/pages/RedirectUser/index.js
+++ frontend/src/pages/RedirectUser/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
-// import {useHistory} from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
 
 export default function RedirectUser(){
+    const history = useHistory();
+
+    /*-----------------------------------------------------*/
+    //funções
+
+    /*-----------------------------------------------------*/
     return(
         <div>
-            <button>Consultar Amostra</button>
-            <button>Administração</button>
+            <button type={'submit'} onClick={() => history.push('/sample/validate')}>Consultar Amostra</button>
+            <button type={'submit'} onClick={() => history.push('/admin/dashboard')}>Administração</button>
         </div>
     );
 }
