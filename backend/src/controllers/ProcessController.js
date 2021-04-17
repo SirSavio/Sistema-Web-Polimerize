f9201701d3da16934dade4802fc819241ba55979
paiva98 <nilsonpaiva98@gmail.com>
2021-04-17 16:00:22 -0300

    processos com data

--- backend/src/controllers/ProcessController.js
+++ backend/src/controllers/ProcessController.js
@@ -1,13 +1,13 @@
 const connection = require('../database/connection');
 
-// const data = new Date();
+const data = new Date();
 
 module.exports = {
     async index(request, response){
         const {id_sample} = request.params; 
 
         const res = await connection('process')
-            .select('id','name','describe')
+            .select('id','name','describe', "id_sample")
             .where('id_sample',id_sample)
         ;
 
@@ -15,16 +15,23 @@ module.exports = {
     },
     async create(request, response){
         const {name, describe, id_sample} = request.body;
+        const date = data.getFullYear() + '-' + (data.getMonth()+1) + '-' + (data.getDate()+1);
 
-        await connection('process')
+        const res = await connection('process')
             .insert({
                 name: name,
                 describe: describe,
+                date: date,
                 id_sample: id_sample
             })
         ;
 
-        return response.status(200).json({});
+        if(res){
+            return response.status(200).json({});
+        }
+        else{
+            return response.status(500).json({});
+        }
     },
     async change(request, response){
         const {id, name, describe} = request.body;
--- backend/src/database/migrations/20210329051323_create_process.js
+++ backend/src/database/migrations/20210329051323_create_process.js
@@ -3,6 +3,7 @@ exports.up = function(knex) {
         table.increments('id');
         table.string('name', 60).notNullable();
         table.string('describe', 255).notNullable();
+        table.date('date').notNullable();
 
         table.integer('id_sample').unsigned().notNullable();
         table.foreign('id_sample').references('id').inTable('sample');
