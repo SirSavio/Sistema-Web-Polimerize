7beabdf24f0f21a595a391e33fb294cf1d877ada
paiva98 <nilsonpaiva98@gmail.com>
2021-04-19 05:30:16 -0300

    Alterar processos implementado

--- frontend/src/pages/DashboardAdmin/components/Process.js
+++ frontend/src/pages/DashboardAdmin/components/Process.js
@@ -2,6 +2,7 @@ import React,{useState, useEffect} from 'react';
 import api from '../../../services/api';
 export default function Process({id}){
     const [process, setProcess] = useState([]);
+    const [processChange, setProcessChange] = useState(false);
     var D = new Date();
 
     useEffect(()=>{
@@ -10,23 +11,60 @@ export default function Process({id}){
                 setProcess(responce.data);
             })
         ;
-    },[id])
+    },[id,setProcess])
+
+    async function change(e){
+        e.preventDefault();
+
+        const id = e.target.id.value;
+        const name = e.target.name.value;
+        const describe = e.target.describe.value;
+
+        const dat = {name,describe,id};
+
+        try{
+            await api.put('/sample/process', dat);
+            (processChange)? setProcessChange(false): setProcessChange(true);
+            alert("Dados do processo atualizado");
+        }
+        catch(error){
+            (processChange)? setProcessChange(false): setProcessChange(true);
+            alert("Erro ao atulizar os dados do processo")
+        }
+    }
 
     return (
         <div>
             {process.map((pro) => (
                 <div key={pro.id}>
-                    <label htmlFor="">Processo: </label>
-                    <span>{pro.name}</span>
-                    <br/>
-
-                    <label htmlFor="">Descrição: </label>
-                    <span>{pro.describe}</span>
-                    <br/>
-
-                    <label htmlFor="">Data: </label>
-                    <span>{ D.toISOString(pro.date) }</span>
-                    <br/>
+                    <form onSubmit={change}>
+                        <input 
+                            type="hidden"
+                            name={"id"}
+                            required
+                            defaultValue={pro.id}
+                        />
+                        <br/>
+                        <input 
+                            type="text"
+                            name={"name"}
+                            placeholder={pro.name}
+                            required
+                            defaultValue={pro.name}
+                        />
+                        <br/>
+                        <textarea
+                            type="text"
+                            name={"describe"}
+                            placeholder={pro.describe}
+                            required
+                            defaultValue={pro.describe}
+                        />
+                        <br/>
+                        <span>{ D.toISOString(pro.date) }</span>
+                        <br/>
+                        <button type={'submit'}>Salvar</button>
+                    </form>
                 </div>
             ))}
         </div>
